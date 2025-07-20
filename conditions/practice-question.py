21bfe2b0fbcfe7e7761801964d1a2f283927eefe
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:44:03 +0530

    Create practice-question.py

---
+++ conditions/practice-question.py
@@ -0,0 +1,12 @@
+#question grade marks
+# 
+ marks =int(input("enter marks:"))
+
+ if(marks >= 90):
+     print("a")
+ elif(marks >= 80 and marks < 90):
+     print("b")
+ elif(marks >= 70 and marks < 80):
+     print("c")
+ elif(70 > marks):
+   print("d")            
