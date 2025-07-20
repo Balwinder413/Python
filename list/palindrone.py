35fc8867a14b978802b73bb17f2cf1a9b2d2cd75
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 17:13:02 +0530

    Create palindrone.py

---
+++ list/palindrone.py
@@ -0,0 +1,22 @@
+#palindrone
+
+list=[1,2,1]
+
+copy_list =list.copy()
+copy_list.reverse()
+
+if(copy_list == list):
+    print(" palindrone")
+else:
+    print("not palindrone")
+
+
+#
+letter=["m","a","m"]
+copy_letter= letter.copy()
+copy_letter.reverse()
+
+if(copy_letter == letter):
+    print("palindrone")
+else: 
+    print("not palindrone")
