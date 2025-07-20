b907d104816a2d259e825edccfb578851af74c66
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:37:50 +0530

    Create first-program.py

---
+++ basic/first-program.py
@@ -0,0 +1,24 @@
+name="balwinder kaur"
+age=21
+price=45
+print("my nameis:", name)
+print("my age is:", age)
+
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# string 
+name1="veer ji"
+name2='veer ji'
+name3='''veer ji'''
+print(name1)
+print(name2)
+
+
+age=34
+old= False
+a=None
+print(type(age))
+print(type(old))
