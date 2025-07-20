6d81284df2bf0a4f6ef4baf7b89ab69cb1aa61de
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 17:11:30 +0530

    Create method.py

---
+++ list/method.py
@@ -0,0 +1,28 @@
+# methods
+list=[1,2,3,4,5,6,7,7,8,90,]
+(list.append(2))# add value
+print(list)
+
+# sort 
+list.sort()
+print(list)
+
+#reverse sort
+list.sort(reverse=True)
+print(list)
+
+# reverse
+list.reverse()
+print(list)
+
+#insert
+list.insert(2,6)
+print(list)
+
+#remove
+list.remove(6)
+print(list)
+
+#pop
+list.pop(0)
+print(list)
