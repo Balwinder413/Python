270010157d56c3e300a2d3ef43d085b42814b860
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 17:03:59 +0530

    Create basic--2.py

---
+++ list/basic--2.py
@@ -0,0 +1,21 @@
+print(marks[0])
+print(marks[6])
+
+# len
+print(len(marks))
+
+
+
+# store element differnt types
+student=[45, "balwinder", "ghangas", 23 ]
+print(student)
+print(len(student))
+print(student[2])
+print(type(student))
+
+
+#list are mutable
+hero=["ammy","amrinder","gippy"]
+print(hero)
+hero[1]="sippy gill"
+print(hero)
