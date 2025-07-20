e2a966e6ec221ff9fd9686c31becf2268ffcbdea
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:53:18 +0530

    Create practice-ques.py

---
+++ string/practice-ques.py
@@ -0,0 +1,7 @@
+#wap to input user first no&print its length 
+name=input("enter the name:")
+print("length of name is",len(name))
+
+#wap find the occurance of "r" im string 
+str3= "hi i am retu"
+print(str3.count("r"))
