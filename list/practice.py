757b3b9d3cbea93f8b2941b34a37209a9cc4a48d
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 17:12:07 +0530

    Create practice.py

---
+++ list/practice.py
@@ -0,0 +1,11 @@
+wap to user enter names of their 3 fav movies & store in list
+movies=[]
+movie1= input("enter the first movie ")
+movie2= input("enter the second movie")
+movie3= input("enter the third movie")
+
+(movies.append(movie1))
+(movies.append(movie2))
+(movies.append(movie3))
+
+print(movies)
