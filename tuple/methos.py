8bfe28591795a1807fe66f034a30457504ff6bc2
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 17:16:43 +0530

    Create methos.py

---
+++ tuple/methos.py
@@ -0,0 +1,13 @@
+# methods
+# index
+tup4=(2,3,4,5,6,7)
+print(tup4)
+print(tup4[2])
+print(type(tup4))
+print(tup4.index(5))
+
+tup5=(2,3,4,5,6,7)
+print(tup5)
+print(tup5[2])
+print(type(tup5))
+print(tup5.count(3))
