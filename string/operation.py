a89c452e9500b3ddf54b760e471d1643dfd81369
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:51:57 +0530

    Create operation.py

---
+++ string/operation.py
@@ -0,0 +1,25 @@
+#opertaion concatenation
+str1= "balwinder kaur"
+str2="ghangas"
+print(str1+str2)
+
+#len(str)
+str1= "balwinder kaur"
+str2="ghangas"
+print(len(str1))
+
+#indexing 
+str1= "balwinder kaur"
+str2="ghangas"
+print(str1[0])
+
+#slicing
+str1= "balwinder kaur"
+str2="ghangas"
+print(str2[1:4])
+
+
+#negative
+str1= "balwinder kaur"
+str2="ghangas"
+print(str1[-4:-1])
