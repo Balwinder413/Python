919e9982e56712b0f80ee9dc01ff07666f07b3cd
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:42:37 +0530

    Create basic.py

---
+++ conditions/basic.py
@@ -0,0 +1,19 @@
+# conditional statement 
+# syntax if(condition):
+# statement1
+age=21
+if(age>=18):
+    print("adult")
+
+#     elif(condition):
+#         statement
+
+light = "yellow"
+
+if(light == "green"):
+    print("go")
+elif(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+
