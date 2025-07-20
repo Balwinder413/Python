7db4658b8776b86a43b76491f2df5c9548dee790
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-20 13:39:16 +0530

    Create practice-ques.py

---
+++ basic/practice-ques.py
@@ -0,0 +1,14 @@
+#wap to input 2no and print sum
+a= int (input("first no:"))
+b=int (input("second no:"))
+sum=a+b
+print(sum)
+
+#wap input side square and print area
+side=int(input("enter square:"))
+print ("area=",side*side)
+
+#wap to input 2 floting no and print average
+a=float(input("enter first :"))
+b=float(input("enter second:"))
+print("average=",a+b/2)
