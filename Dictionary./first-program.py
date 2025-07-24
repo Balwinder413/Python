703e7459dd49ed60aa287851b62b3592d80d2d05
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 12:04:09 +0530

    Create first-program.py

---
+++ Dictionary./first-program.py
@@ -0,0 +1,18 @@
+info={
+    "name":"balwinder",
+    "learning":"coding",
+    "age":"23",
+    
+    "topic":("tuple","list")
+}
+print(info)
+print(type(info))
+print(info["name"])
+info["name"]="retu"
+print(info["name"])
+print(len(info))
+
+# empty dic
+null_dict={}
+print(null_dict)
+print(len(null_dict))
