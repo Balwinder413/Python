732ada3cfc27f71babc7606ccd8319f1d317565c
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 21:39:47 +0530

    Create practice01.py

---
+++ Dictionary./practice01.py
@@ -0,0 +1,9 @@
+store words python dict:
+#table:"a piece of furniture","list of fact and figure"
+# cat:"a small animal"
+
+dict={
+    "table":["a piece of furniture","list of fact and figure"],
+    "cat":"a small animal"
+}
+print(dict)
