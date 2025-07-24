6b5bc27ba4891ae2c18fc678701c94ea936f90ae
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 21:43:14 +0530

    Create add-method.py

---
+++ Sets/add-method.py
@@ -0,0 +1,12 @@
+# add method
+heroes={"champo","pampo","bhaiyoo","2","4"}
+print(heroes)
+print(len(heroes))
+print(type(heroes))
+heroes.add("ranjeet veer ji")
+print(heroes)
+
+
+heroes.add("4")
+print(heroes)
+print(len(heroes))
