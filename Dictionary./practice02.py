a57e53a82a2605c004c4c66d66d20dec45f60b20
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 21:40:30 +0530

    Create practice02.py

---
+++ Dictionary./practice02.py
@@ -0,0 +1,20 @@
+#
+marks={}
+
+x= int(input("enter physics:"))
+marks.update({"physics": x})
+
+x= int(input("enter math:"))
+marks.update({"math": x})
+
+x= int(input("enter chemis:"))
+marks.update({"chemis": x})
+print(marks)
+
+
+#
+values={
+    ("int",9),
+    ("float",9)
+}
+print(values)
