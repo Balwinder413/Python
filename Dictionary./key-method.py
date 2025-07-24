7a6d111bce0e24681f202b7c4148e4ab8af6c86c
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 12:05:17 +0530

    Create key-method.py

---
+++ Dictionary./key-method.py
@@ -0,0 +1,10 @@
+#.keys  return all keys
+student={
+    "name":"balwinder kaur",
+    "subject":{
+        "physics":"56",
+        "chemis":"78"
+    }
+}
+print(student.keys())
+print(len(student))
