c43ee36bf161202d68f65b6ab4eb69781f1de422
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 12:06:33 +0530

    Create items-method.py

---
+++ Dictionary./items-method.py
@@ -0,0 +1,9 @@
+#.items  return all (key,val)pair as tuples
+student={
+    "name":"balwinder kaur",
+    "subject":{
+        "physics":"56",
+        "chemis":"78"
+    }
+}
+print(student.items())
