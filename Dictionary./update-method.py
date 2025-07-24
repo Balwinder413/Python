b118ba673b63abbbba6b72b0cc32470aa201874b
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-24 12:07:43 +0530

    Create update-method.py

---
+++ Dictionary./update-method.py
@@ -0,0 +1,10 @@
+#.update  insert items
+student={
+    "name":"balwinder kaur",
+    "subject":{
+        "physics":"56",
+        "chemis":"78"
+    }
+}
+student.update({"city": "delhi"})
+print(student)
